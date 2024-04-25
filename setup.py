8103b085cc5604d86c2082566d60cc9f3bdc47da
Jacek Banaszczyk <jacek.banaszczyk@gmail.com>
2024-04-25 11:08:57 +0200

    Minor fixes

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 setup(
     name="micropython-phew",
-    version="0.0.2",
-    description="A small webserver and templating library specifically designed for MicroPython on the Pico W.",
+    version="0.0.4",
+    description="Webserver and templating library specifically designed for MicroPython on the Pico W. (C&Q update)",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     project_urls={
-        "GitHub": "https://github.com/pimoroni/phew"
+        "GitHub": "https://github.com/c-and-q/phew"
     },
-    author="Jonathan Williamson",
-    maintainer="Phil Howard",
-    maintainer_email="phil@pimoroni.com",
+    author="Jonathan Williamson (pimoroni)",
+    maintainer="Jacek Banaszczyk",
+    maintainer_email="jacek.banaszczyk@gmail.com",
     license="MIT",
     license_files="LICENSE",
     packages=["phew"]
